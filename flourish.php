a2c42e3c135e414e78bbd73083c166712dd96608
Jonathan Lindgren <jonathan@novastream.se>
2013-10-25 23:58:40 +0200

    initial release

---
+++ flourish.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'flourish/fCore.php';
+include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'flourish/fEmail.php';
+include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'flourish/fMailbox.php';
+
+class flourish {
+	public function __construct() {
+		
+	}
+}
